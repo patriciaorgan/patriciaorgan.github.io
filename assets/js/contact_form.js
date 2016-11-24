7e9b3563cb9196db19365ed9f8428b790b513908
Patricia Organ <trishaorgan@trishas-mbp.attlocal.net>
2016-11-23 23:07:46 -0600

    remove php
    php does not work in github pages, used simple mailto instead

--- assets/js/contact_form.js
+++ assets/js/contact_form.js
@@ -8,17 +8,13 @@ $(document).ready(function() {
 	if (name == '' || email == '' || message == '') {
 	  alert("Please Fill Required Fields");
 	} else {
-	  // Returns successful data submission message when the entered information is stored in database.
-	  $.post("assets/php/contact_form.php", {
-	    name1: name,
-	    email1: email,
-	    message1: message,
-	  }, function(data) {
-	    $("#returnmessage").append(data); // Append returned message to message paragraph.
-	    if (data == "Your Query has been received, I will contact you soon.") {
-	      $("#form")[0].reset(); // To reset form fields on success.
-	    }
-	  });
+	  var template = 'Thanks ' + name +'! For Contacting me.\n\n'
+		+ 'Name: ' + name + '\n'
+		+ 'Email: ' + email + '\n'
+		+ 'Message: ' + message + '\n';
+	  var mailto = "mailto:patriciaorgan82@gmail.com,"+ email + "?subject=From Portfolio website: "
+	    + name + " , " + email + "&body=" + encodeURIComponent(template);
+	  document.location.href =mailto;
 	}
   });
 });
