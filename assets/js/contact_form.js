fb7e37185932c22c441ae60923b92b5e95d4d3d5
Patricia Organ <trishaorgan@trishas-mbp.attlocal.net>
2016-11-25 14:00:41 -0600

    reset contact form

--- assets/js/contact_form.js
+++ assets/js/contact_form.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	// Checking for blank fields.
 	if (name == '' || email == '' || message == '') {
 	  alert("Please Fill Required Fields");
+	} else if (!validateEmail(email)) {
 	} else {
 	  var template = 'Thanks ' + name +'! For Contacting me.\n\n'
 		+ 'Name: ' + name + '\n'
@@ -14,7 +15,20 @@ $(document).ready(function() {
 		+ 'Message: ' + message + '\n';
 	  var mailto = "mailto:patriciaorgan82@gmail.com,"+ email + "?subject=From Portfolio website: "
 	    + name + " , " + email + "&body=" + encodeURIComponent(template);
-	  document.location.href =mailto;
+	  document.location.href = mailto;
+	  //reset form
+	  document.getElementById("form").reset();
 	}
   });
 });
+
+function validateEmail(email) {
+    var atpos = email.indexOf("@");
+    var dotpos = email.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    } else {
+    	return true;
+    }
+}
